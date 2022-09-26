4c9b88be729fe9e40f39aadb82ad41c2b40fab09
Florian Keruzore <fkeruzore@anl.gov>
2022-09-26 12:16:17 -0500

    useless file

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""Top level package for panco2"""
-
-__author__ = "Florian Keruzore"
-__email__ = "fkeruzore@anl.gov"
-
-from .panco2 import PressureProfileFitter
-from . import results, noise_covariance, utils
